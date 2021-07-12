97c252f4bdaca42ded359c6e596f43fc08c9d5e5
Marcos Machado <47403470+MarcosMarchado@users.noreply.github.com>
2021-07-12 20:20:21 -0300

    Corrigindo métodos dentro do Repository de Perguntas e Opiniões

--- src/main/java/br/com/marcos/zupacademy/mercadolivre/produto/repository/OpiniaoRepository.java
+++ src/main/java/br/com/marcos/zupacademy/mercadolivre/produto/repository/OpiniaoRepository.java
@@ -3,8 +3,5 @@ package br.com.marcos.zupacademy.mercadolivre.produto.repository;
 import br.com.marcos.zupacademy.mercadolivre.produto.modelo.OpiniaoDoProduto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface OpiniaoRepository extends JpaRepository<OpiniaoDoProduto, Long> {
-    List<OpiniaoDoProduto> findByProdutoId(Long id);
 }
--- src/main/java/br/com/marcos/zupacademy/mercadolivre/produto/repository/PerguntaRepository.java
+++ src/main/java/br/com/marcos/zupacademy/mercadolivre/produto/repository/PerguntaRepository.java
@@ -3,8 +3,5 @@ package br.com.marcos.zupacademy.mercadolivre.produto.repository;
 import br.com.marcos.zupacademy.mercadolivre.produto.modelo.Pergunta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface PerguntaRepository extends JpaRepository<Pergunta, Long> {
-    List<Pergunta> findByProdutoId(Long id);
 }
